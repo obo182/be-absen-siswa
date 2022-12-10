59e6f538b3dc1e933cd134c3856301c4437ed4f3
wahyurahmana <tn.wahyurahmana@gmail.com>
2022-12-11 05:59:32 +0800

    api absensi siswa

--- controllers/SiswaController.js
+++ controllers/SiswaController.js
@@ -3,9 +3,7 @@ const {Siswa, DataAbsen} = require('../models/index.js')
 module.exports = class SiswaController {
   static async getAll(req, res, next){
     try {
-      const result = await Siswa.findAll({
-        include : [DataAbsen]
-      })
+      const result = await Siswa.findAll()
       res.status(200).json(result)
     } catch (error) {
       next(error)
@@ -54,6 +52,36 @@ module.exports = class SiswaController {
         throw {status : 404, message : `Siswa Id ${req.params.idSiswa} Not Found`}
       }
       res.status(200).json({message : `Succes Update Siswa Id ${req.params.idSiswa}`})
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async detailSiswa(req, res, next){
+    try {
+      try {
+        const result = await Siswa.findOne({
+          where : {
+            id : req.params.idSiswa
+          },
+          include : [DataAbsen]
+        })
+        res.status(200).json(result)
+      } catch (error) {
+        next(error)
+      }
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async absensi(req, res, next){
+    try {
+      const {id_siswa, status} = req.body
+      const absen = await DataAbsen.create({
+        id_siswa, status
+      })
+      res.status(201).json(absen)
     } catch (error) {
       next(error)
     }
